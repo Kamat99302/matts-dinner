48d551a5aa6c1ab1ad9d3976007c7e0989169298
Kamat99302 <matthieu.juan@gmail.com>
2026-02-16 22:02:07 +0100

    First commit
    - Projet setup
    - Create StartScreen component with background image
    - Add 'Touch to order' button using library Button component
    - Style with Bebas Neue font matching Figma design
    - First screen of Matt's Diner app complete"

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import StartScreen from './components/StartScreen';
+import 'matts-dinner-component-library/dist/matts-dinner-component-library.css';
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+     <StartScreen/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,24 @@
+import './StartScreen.css'
+import startScreenBg from '../assets/images/bg-startScreen.png'
+import { Button } from 'matts-dinner-component-library'
+export default function StartScreen(){
+    return(
+        <div
+        style={{backgroundImage: `url(${startScreenBg})`}}
+        className='start-screen-container'>
+            <div className='order-here'>
+                <span className='order'>ORDER</span>
+                <span className='here'>HERE</span>
+            </div>
+            <div className='touch-to-order-btn'>
+                <Button variant="primary-black" size="large">
+                    Touch to order
+                </Button>
+            </div>
+
+        </div>
+        
+        
+        
+    )
+}
